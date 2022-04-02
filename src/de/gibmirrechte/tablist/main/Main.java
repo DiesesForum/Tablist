cc76e97ae27a6c7df6afd63461e365e83c62ed4f
GibMirRechte <55894240+DiesesForum@users.noreply.github.com>
2022-04-02 21:34:41 +0200

    Initial commit

---
+++ src/de/gibmirrechte/tablist/main/Main.java
@@ -0,0 +1,11 @@
+package de.gibmirrechte.tablist.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("");
+    }
+}
